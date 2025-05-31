27d3c59b920f47a298c8977c098d50d96484384d
Gonçalo_Rodrigues <G.dRCPRO@protonmail.com>
2025-05-31 11:42:27 +0100

    Separated the main with the algorithms

---
+++ Client.java
@@ -0,0 +1,47 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Which elementary algoritms you want to use? 1 - Bubble sort");
+        int choice = scanner.nextInt();
+        System.out.println("Which array size you want to use?");
+        int size = scanner.nextInt();
+        Random random = new Random();
+
+        double averageMS = 0;
+
+        for (int i = 0; i < 100; i++) {
+            int[] array = new int[size];
+
+            for (int j = 0; j < array.length; j++) {
+                array[j] = random.nextInt(0, 1001);
+            }
+
+            long startTime;
+            long endTime;
+
+            switch (choice) {
+                case (1):
+                    startTime = System.nanoTime();
+                    ElementarySortingAlgorithms.myBubbleSort(array);
+                    endTime = System.nanoTime();
+                    break;
+                default:
+                    startTime = System.nanoTime();
+                    ElementarySortingAlgorithms.myBubbleSort(array);
+                    endTime = System.nanoTime();
+                    break;
+            }
+
+            double durationNano = endTime - startTime;
+            double durationMillis = durationNano / 1_000_000.0;
+
+            averageMS += durationMillis;
+        }
+
+        System.out.println("Sorting took, on average, " + averageMS/100 + " ms");
+    }
+}
--- ElementaryAlgorithms/src/ElementarySortingAlgorithms.java
+++ ElementaryAlgorithms/src/ElementarySortingAlgorithms.java
@@ -1,5 +1,5 @@
-import java.util.Random;
-import java.util.Scanner;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class ElementarySortingAlgorithms {
 
@@ -26,55 +26,5 @@ public class ElementarySortingAlgorithms {
             }
             originalNumberOfSwaps = numberOfSwaps;
         }
-    }
-
-    public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Which elementary algoritms you want to use? 1 - Bubble sort");
-        int choice = scanner.nextInt();
-        System.out.println("Which array size you want to use?");
-        int size = scanner.nextInt();
-        Random random = new Random();
-
-        double averageNS = 0;
-        double averageSC = 0;
-        double averageMS = 0;
-
-        for (int i = 0; i < 100; i++) {
-            int[] array = new int[size];
-
-            for (int j = 0; j < array.length; j++) {
-                array[j] = random.nextInt(0, 1001);
-            }
-
-            long startTime;
-            long endTime;
-
-            switch (choice) {
-                case (1):
-                    startTime = System.nanoTime();
-                    myBubbleSort(array);
-                    endTime = System.nanoTime();
-                    break;
-                default:
-                    startTime = System.nanoTime();
-                    myBubbleSort(array);
-                    endTime = System.nanoTime();
-                    break;
-            }
-
-            double durationNano = endTime - startTime;
-            double durationSeconds = durationNano / 1_000_000_000.0;
-            double durationMillis = durationNano / 1_000_000.0;
-
-            averageNS += durationNano;
-            averageSC += durationSeconds;
-            averageMS += durationMillis;
-        }
-
-        System.out.println("Sorting took, on average, " + averageNS/100 + " ns");
-        System.out.println("Sorting took, on average, " + averageSC/100 + " sec");
-        System.out.println("Sorting took, on average, " + averageMS/100 + " ms");
     }
 }
